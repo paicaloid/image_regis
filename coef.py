94fcba99a83b4d8b5608356c89fa98e88c485f2c
Paipp <paicaloid@gmail.com>
2018-11-10 18:48:17 +0700

    add loop coef

--- coef.py
+++ coef.py
@@ -6,28 +6,94 @@ import sonarGeometry
 import regisProcessing as rp
 from matplotlib import pyplot as plt
 
+def coefShift(img1, img2, num):
+    colRange = np.arange((-1)*num, num+1, 1)
+    rowRange = colRange * (-1)
+    rowList = []
+    coefList = []
+    for rowInx in rowRange:
+        colList = []
+        for colInx in colRange:
+            trans_matrix = np.float32([[1,0,colInx],[0,1,rowInx]])
+            imgShift = cv2.warpAffine(img2, trans_matrix, (768,500))
+
+            # ! Quadrant 1
+            if rowInx > 0 and colInx > 0:
+                imgRef = img1[rowInx:500, colInx:768]
+                imgShift = imgShift[rowInx:500, colInx:768]
+            # ! Quadrant 2
+            elif rowInx > 0 and colInx < 0:
+                imgRef = img1[rowInx:500, 0:768+colInx]
+                imgShift = imgShift[rowInx:500, 0:768+colInx]
+            # ! Quadrant 3
+            elif rowInx < 0 and colInx < 0:
+                imgRef = img1[0:500+rowInx, 0:768+colInx]
+                imgShift = imgShift[0:500+rowInx, 0:768+colInx]
+            # ! Quadrant 4
+            elif rowInx < 0 and colInx > 0:
+                imgRef = img1[0:500+rowInx, colInx:768]
+                imgShift = imgShift[0:500+rowInx, colInx:768]
+            
+            coef = cv2.matchTemplate(imgRef, imgShift, cv2.TM_CCOEFF_NORMED)
+            colList.append(coef[0][0])
+        coefList.append(max(colList))
+        rowList.append((rowInx, max(enumerate(colList), key=(lambda x: x[1]))))
+        # print (rowInx, max(enumerate(colList), key=(lambda x: x[1])))
+    # print (coefList)
+    # print (rowList)
+    print (rowList[np.argmax(coefList)])
+    posShift = rowList[np.argmax(coefList)]
+    shiftRow = posShift[0]
+    shiftCol = posShift[1][0] - num
+    print (shiftRow, shiftCol)
+
 if __name__ == '__main__':
-    img1 = rp.AverageMultiLook(10,1)
-    img2 = rp.AverageMultiLook(15,1)
-    # trans_matrix = np.float32([[1,0,0],[0,1,-16]])
-    # out = cv2.warpAffine(img2, trans_matrix, (768,500))
+    # img1 = rp.AverageMultiLook(10,1)
+    # img2 = rp.AverageMultiLook(11,1)
 
-    ### Test R ###
-    # for inx in range(-20,21):
-    #     trans_matrix = np.float32([[1,0,0],[0,1,inx]])
-    #     imgShift = cv2.warpAffine(img2, trans_matrix, (768,500))
-    #     if inx < 0:
-    #         imgRef = img1[0:500+inx, 0:768]
-    #         imgShift = imgShift[0:500+inx, 0:768]
-    #     else:
-    #         imgRef = img1[inx:500, 0:768]
-    #         imgShift = imgShift[inx:500, 0:768]
+    # coefShift(img1, img2, 10)
 
-    #     coef = cv2.matchTemplate(imgRef, imgShift, cv2.TM_CCOEFF_NORMED)
-    #     print (inx, coef[0][0])
+    ### Test loop find shift ###
+    if True:
+        inx = np.arange(11, 21, 1)
+        for num in inx:
+            img1 = rp.AverageMultiLook(num-1, 1)
+            img2 = rp.AverageMultiLook(num, 1)
+            coefShift(img1, img2, 10)
+            print ("===============")
+
+    ### Test add image ###
+    if False:
+        img3 = rp.AverageMultiLook(12,1)
+        img4 = rp.AverageMultiLook(13,1)
+        img5 = rp.AverageMultiLook(14,1)
+        img6 = rp.AverageMultiLook(15,1)
+
+        trans_matrix = np.float32([[1,0,0],[0,1,-1]])
+        img2 = cv2.warpAffine(img2, trans_matrix, (768,500))
+        trans_matrix = np.float32([[1,0,0],[0,1,-2]])
+        img3 = cv2.warpAffine(img3, trans_matrix, (768,500))
+        trans_matrix = np.float32([[1,0,0],[0,1,-4]])
+        img4 = cv2.warpAffine(img4, trans_matrix, (768,500))
+        trans_matrix = np.float32([[1,0,-1],[0,1,-5]])
+        img5 = cv2.warpAffine(img5, trans_matrix, (768,500))
+        trans_matrix = np.float32([[1,0,-2],[0,1,-6]])
+        img6 = cv2.warpAffine(img6, trans_matrix, (768,500))
+
+        cv2.imshow("0", img1)
+        img1 = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
+        cv2.imshow("1", img1)
+        img1 = cv2.addWeighted(img1, 0.5, img3, 0.5, 0)
+        cv2.imshow("2", img1)
+        img1 = cv2.addWeighted(img1, 0.5, img4, 0.5, 0)
+        cv2.imshow("3", img1)
+        img1 = cv2.addWeighted(img1, 0.5, img5, 0.5, 0)
+        cv2.imshow("4", img1)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
 
     ### Test specific ###
-    if True:
+    if False:
         posRow = -6
         posCol = -2
         trans_matrix = np.float32([[1,0,posCol],[0,1,posRow]])
