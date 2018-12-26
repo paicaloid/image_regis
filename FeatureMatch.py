e9cae5392156127efbafd125e1963c803bcf8b88
Paipp <paicaloid@gmail.com>
2018-12-26 12:58:28 +0700

    Update FeatureMatch.py
    edit FLANN

--- FeatureMatch.py
+++ FeatureMatch.py
@@ -6,7 +6,7 @@ def matchPosition_BF(img1, img2, savename):
     ref_Position = []
     shift_Position = []
     try:
-        # Initiate SIFT detector
+        ## Initiate SIFT detector
         nfeatures = 0
         nOctaveLayers = 3
         contrastThreshold = 0.04
@@ -15,32 +15,22 @@ def matchPosition_BF(img1, img2, savename):
         # sift = cv2.xfeatures2d.SIFT_create(nfeatures, nOctaveLayers, contrastThreshold, edgeThreshold, sigma)
         sift = cv2.xfeatures2d.SIFT_create()
 
-        # find the keypoints and descriptors with SIFT
+        ## find the keypoints and descriptors with SIFT
         kp1, des1 = sift.detectAndCompute(img1,None)
         kp2, des2 = sift.detectAndCompute(img2,None)
 
-        # BFMatcher with default params
+        ## BFMatcher with default params
         bf = cv2.BFMatcher()
         matches = bf.knnMatch(des1,des2, k=2)
 
-        # Apply ratio test
+        ## Apply ratio test
         good = []
-        
         inx = 0
-    
         for m,n in matches:
-            # print (m.distance, n.distance)
-            # print (m.imgIdx, n.imgIdx)
-            # print (m.queryIdx, n.queryIdx)
-            # print (m.trainIdx, n.trainIdx)
-
             if m.distance < 0.75*n.distance:
                 good.append([m])
                 ref_Position.append(kp1[m.queryIdx].pt)
                 shift_Position.append(kp2[m.trainIdx].pt)
-                # print (kp1[inx].pt)
-                # print (kp2[inx].pt)
-        # print (ref_Position, shift_Position)
         img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
         if savename != "0":
             plt.imshow(img3),plt.savefig(savename)
@@ -51,29 +41,24 @@ def matchPosition_BF(img1, img2, savename):
         return ref_Position, shift_Position
 
 def BF_matching(img1, img2):
-    # Initiate SIFT detector
+    ## Initiate SIFT detector
     sift = cv2.xfeatures2d.SIFT_create()
 
-    # find the keypoints and descriptors with SIFT
+    ## find the keypoints and descriptors with SIFT
     kp1, des1 = sift.detectAndCompute(img1,None)
     kp2, des2 = sift.detectAndCompute(img2,None)
 
-    # BFMatcher with default params
+    ## BFMatcher with default params
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1,des2, k=2)
 
-    # Apply ratio test
+    ## Apply ratio test
     good = []
     inx = 0
     for m,n in matches:
         if m.distance < 0.75*n.distance:
             good.append([m])
-            # print (kp1[inx].pt)
-            # print (kp2[inx].pt)
-            # print (m.trainIdx, n.trainIdx)
-            # print (m.queryIdx, n.queryIdx)
-            # print (m.imgIdx, n.imgIdx)
-    # cv2.drawMatchesKnn expects list of lists as matches.
+    ## cv2.drawMatchesKnn expects list of lists as matches.
     img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
 
     plt.imshow(img3),plt.show()
@@ -161,49 +146,60 @@ def FLANN_matching(img1, img2):
     img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
     plt.imshow(img3,),plt.show()
     
-def FLANN_saveMatching(img1, img2, filename):
-    # Initiate SIFT detector
+def FLANN_saveMatching(img1, img2, savename):
+    ## Initiate SIFT detector
+    ref_Position = []
+    shift_Position = []
+    try:
+        ### SIFT parameter ###
+        ## nfeatures : The number of best features to retain
+        ## nOctaveLayers : computed automatically from the image resolution
+        ## contrastThreshold : used to filter out weak features. 
+        ##                     The larger the threshold, the less features are produced by the detector.
+        ## edgeThreshold : used to filter out edge-like features.
+        ##                 the larger the edgeThreshold, the less features are filtered out (more features are retained)
+        ## sigma : The sigma of the Gaussian applied to the input image.
+        ##         If your image is captured with a weak camera with soft lenses, you might want to reduce the number.
+        nfeatures = 0
+        nOctaveLayers = 3
+        contrastThreshold = 0.04
+        edgeThreshold = 5
+        sigma = 1.6
+        # sift = cv2.xfeatures2d.SIFT_create(nfeatures, nOctaveLayers, contrastThreshold, edgeThreshold, sigma)
+        sift = cv2.xfeatures2d.SIFT_create()
 
-    ### SIFT parameter ###
-    ## nfeatures : The number of best features to retain
-    ## nOctaveLayers : computed automatically from the image resolution
-    ## contrastThreshold : used to filter out weak features. 
-    ##                     The larger the threshold, the less features are produced by the detector.
-    ## edgeThreshold : used to filter out edge-like features.
-    ##                 the larger the edgeThreshold, the less features are filtered out (more features are retained)
-    ## sigma : The sigma of the Gaussian applied to the input image.
-    ##         If your image is captured with a weak camera with soft lenses, you might want to reduce the number.
-    nfeatures = 0
-    nOctaveLayers = 3
-    contrastThreshold = 0.04
-    edgeThreshold = 5
-    sigma = 1.6
-    # sift = cv2.xfeatures2d.SIFT_create(nfeatures, nOctaveLayers, contrastThreshold, edgeThreshold, sigma)
-    sift = cv2.xfeatures2d.SIFT_create()
+        ## find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
 
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1,None)
-    kp2, des2 = sift.detectAndCompute(img2,None)
+        ## FLANN parameters
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks=50)   # or pass empty dictionary
+        flann = cv2.FlannBasedMatcher(index_params,search_params)
+        matches = flann.knnMatch(des1,des2,k=2)
 
-    # FLANN parameters
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks=50)   # or pass empty dictionary
-    flann = cv2.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
+        ## Need to draw only good matches, so create a mask
+        matchesMask = [[0,0] for i in range(len(matches))]
 
-    # Need to draw only good matches, so create a mask
-    matchesMask = [[0,0] for i in range(len(matches))]
+        ## ratio test as per Lowe's paper
+        for i,(m,n) in enumerate(matches):
+            if m.distance < 0.75*n.distance:
+                matchesMask[i]=[1,0]
+                # print (i)
+                ref_Position.append(kp1[m.queryIdx].pt)
+                shift_Position.append(kp2[m.trainIdx].pt)
+        draw_params = dict(matchColor = (0,255,0),
+                        singlePointColor = (255,0,0),
+                        matchesMask = matchesMask,
+                        flags = 0)
 
-    # ratio test as per Lowe's paper
-    for i,(m,n) in enumerate(matches):
-        if m.distance < 0.75*n.distance:
-            matchesMask[i]=[1,0]
-            print (i)
-    draw_params = dict(matchColor = (0,255,0),
-                    singlePointColor = (255,0,0),
-                    matchesMask = matchesMask,
-                    flags = 0)
+        img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
+        if savename != "0":
+            plt.imshow(img3),plt.savefig(savename)
+        elif savename == "0":
+            plt.imshow(img3),plt.show()
+        return ref_Position, shift_Position
+    except:
+        return ref_Position, shift_Position
 
-    img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-    plt.imshow(img3,),plt.savefig(filename)
