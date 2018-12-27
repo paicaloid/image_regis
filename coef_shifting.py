29cde99093c70d2e39ad6e5d198471935a644f71
Paipp <paicaloid@gmail.com>
2018-12-27 16:44:08 +0700

    Create coef_shifting.py

---
+++ coef_shifting.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+def coefShift(img1, img2, num):
+    if(len(img1) == 3):
+        img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    if(len(img2) == 3):
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    
+    colRange = np.arange((-1)*num, num+1, 1)
+    rowRange = colRange * (-1)
+    rowList = []
+    coefList = []
+    row, col = img1.shape
+    for rowInx in rowRange:
+        colList = []
+        for colInx in colRange:
+            trans_matrix = np.float32([[1,0,colInx],[0,1,rowInx]])
+            imgShift = cv2.warpAffine(img2, trans_matrix, (col,row))
+
+            # ! Quadrant 1
+            if rowInx > 0 and colInx > 0:
+                imgRef = img1[rowInx:500, colInx:768]
+                imgShift = imgShift[rowInx:500, colInx:768]
+            # ! Quadrant 2
+            elif rowInx > 0 and colInx < 0:
+                imgRef = img1[rowInx:500, 0:768+colInx]
+                imgShift = imgShift[rowInx:500, 0:768+colInx]
+            # ! Quadrant 3
+            elif rowInx < 0 and colInx < 0:
+                imgRef = img1[0:500+rowInx, 0:768+colInx]
+                imgShift = imgShift[0:500+rowInx, 0:768+colInx]
+            # ! Quadrant 4
+            elif rowInx < 0 and colInx > 0:
+                imgRef = img1[0:500+rowInx, colInx:768]
+                imgShift = imgShift[0:500+rowInx, colInx:768]
+            
+            coef = cv2.matchTemplate(imgRef, imgShift, cv2.TM_CCOEFF_NORMED)
+            colList.append(coef[0][0])
+        coefList.append(max(colList))
+        rowList.append((rowInx, max(enumerate(colList), key=(lambda x: x[1]))))
+    posShift = rowList[np.argmax(coefList)]
+    shiftRow = posShift[0]
+    shiftCol = posShift[1][0] - num
+    print (shiftRow, shiftCol)
+
+    trans_matrix = np.float32([[1,0,shiftCol],[0,1,shiftRow]])
+    img2 = cv2.warpAffine(img2, trans_matrix, (col,row))
+    res = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
+    return res
